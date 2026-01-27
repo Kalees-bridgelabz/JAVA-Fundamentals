0ffceaee108eea91afe3eb399109e54897e67cb8
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-01-27 12:16:51 +0530

    day 6 problem with solutions

---
+++ src/LogicalProgramming/PrimeNumber.java
@@ -0,0 +1,26 @@
+package LogicalProgramming;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 3;
+        int originalNum = n;
+        System.out.print("Prime factors of " + n + " are: ");
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            }
+        }
+
+        if (n == originalNum) {
+            System.out.println(n);
+            System.out.print(n + "is prime number");
+        }else {
+            System.out.println(n);
+            System.out.print(originalNum + "is not prime number");
+
+        }
+
+    }
+
+}
