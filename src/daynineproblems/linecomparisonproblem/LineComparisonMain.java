411016347ef96fc2754c0196db22db24c2878547
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-02-14 11:10:47 +0530

    UC0 Added : Initialization with welcome message

---
+++ src/daynineproblems/linecomparisonproblem/LineComparisonMain.java
@@ -0,0 +1,7 @@
+package daynineproblems.linecomparisonproblem;
+
+public class LineComparisonMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
