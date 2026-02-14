5d0984bea384f2f42384b224f4d83be0e6d1246d
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-02-14 14:41:12 +0530

    UC2 Added : Check equality of two line lengths using equals()

--- src/daynineproblems/linecomparisonproblem/LineComparisonMain.java
+++ src/daynineproblems/linecomparisonproblem/LineComparisonMain.java
@@ -19,8 +19,32 @@ public class LineComparisonMain {
         System.out.print("Enter y2: ");
         int y2 = sc.nextInt();
 
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        Double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
-        System.out.println("Length of the line = " + length);
+        System.out.println("\nEnter coordinates for Line 2");
+
+        System.out.print("Enter x3: ");
+        int x3 = sc.nextInt();
+
+        System.out.print("Enter y3: ");
+        int y3 = sc.nextInt();
+
+        System.out.print("Enter x4: ");
+        int x4 = sc.nextInt();
+
+        System.out.print("Enter y4: ");
+        int y4 = sc.nextInt();
+
+        Double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+
+
+        // Equality Check
+        if (length1.equals(length2)) {
+            System.out.println("Both lines are equal.");
+        } else {
+            System.out.println("Lines are not equal.");
+        }
+
+        //System.out.println("Length of the line = " + length);
     }
 }
