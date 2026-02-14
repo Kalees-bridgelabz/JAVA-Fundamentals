31ae7a65418f03405c20451f4e13922f4e0bbc2a
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-02-14 11:24:25 +0530

    UC1 Added : Calculate line length using distance formula

--- src/daynineproblems/linecomparisonproblem/LineComparisonMain.java
+++ src/daynineproblems/linecomparisonproblem/LineComparisonMain.java
@@ -1,7 +1,26 @@
 package daynineproblems.linecomparisonproblem;
+import java.util.*;
 
 public class LineComparisonMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter x1: ");
+        int x1 = sc.nextInt();
+
+        System.out.print("Enter y1: ");
+        int y1 = sc.nextInt();
+
+        System.out.print("Enter x2: ");
+        int x2 = sc.nextInt();
+
+        System.out.print("Enter y2: ");
+        int y2 = sc.nextInt();
+
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("Length of the line = " + length);
     }
 }
